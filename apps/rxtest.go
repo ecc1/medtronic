3a3cf76b77ecc7a6199c95040348a31abee183a0
Eric Cooper <ecc@cmu.edu>
2016-04-23 15:43:41 -0400

    Add CRC checking

--- apps/rxtest.go
+++ apps/rxtest.go
@@ -8,6 +8,10 @@ import (
 	"github.com/ecc1/spi"
 )
 
+const (
+	verbose = false
+)
+
 func main() {
 	dev, err := spi.Open(cc1100.SpiSpeed)
 	if err != nil {
@@ -33,8 +37,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("\nRF settings after initialization:\n")
-	cc1100.DumpRF(dev)
+	if verbose {
+		fmt.Printf("\nRF settings after initialization:\n")
+		cc1100.DumpRF(dev)
+	}
 
 	for {
 		packet, err := cc1100.ReceivePacket(dev)
@@ -45,19 +51,34 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Received %d bytes (RSSI = %d)\n", len(packet), r)
-		printPacket("raw", packet)
+		if verbose {
+			fmt.Printf("Received %d bytes (RSSI = %d)\n", len(packet), r)
+			printPacket(packet)
+		}
 		data, err := cc1100.Decode6b4b(packet)
 		if err != nil {
-			fmt.Printf("%v\n", err)
-		} else {
-			printPacket("decoded", data)
+			if verbose {
+				fmt.Printf("%v\n", err)
+			}
+			continue
+		}
+		if verbose {
+			printPacket(data)
+		}
+		crc := cc1100.Crc8(data[:len(data)-1])
+		if data[len(data)-1] != crc {
+			if verbose {
+				fmt.Printf("CRC should be %02X, not %02X\n", crc, data[len(data)-1])
+			}
+			continue
+		}
+		if !verbose {
+			printPacket(data)
 		}
 	}
 }
 
-func printPacket(msg string, data []byte) {
-	fmt.Printf("%s:\n", msg)
+func printPacket(data []byte) {
 	for i, v := range data {
 		fmt.Printf("%02X ", v)
 		if (i+1)%20 == 0 {
---
+++ crc8_test.go
@@ -0,0 +1,23 @@
+package cc1100
+
+import (
+	"testing"
+)
+
+func TestCrc8(t *testing.T) {
+	cases := []struct {
+		msg []byte
+		sum byte
+	}{
+		{[]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, 0x98},
+		{[]byte("0123456789"), 0xFD},
+		{[]byte{0xFF, 0xFF, 0xFF, 0xFF}, 0xA3},
+		{[]byte{0x01, 0x07, 0x00, 0x10, 0x04}, 0x44},
+	}
+	for _, c := range cases {
+		sum := Crc8(c.msg)
+		if sum != c.sum {
+			t.Errorf("crc8(%X) == %X, want %X", c.msg, sum, c.sum)
+		}
+	}
+}
