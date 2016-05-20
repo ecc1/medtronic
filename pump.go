7b938e859b4bc267e96f8c4ef63627bfed2288ca
Eric Cooper <ecc@cmu.edu>
2016-05-20 17:51:36 -0400

    Allow frequency to be set via MEDTRONIC_FREQUENCY environment variable

--- pump.go
+++ pump.go
@@ -1,7 +1,15 @@
 package medtronic
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/ecc1/cc1100"
+)
+
+const (
+	freqEnvVar = "MEDTRONIC_FREQUENCY"
 )
 
 type Pump struct {
@@ -20,5 +28,25 @@ func Open() (*Pump, error) {
 	if err != nil {
 		return nil, err
 	}
+	err = r.WriteFrequency(defaultFreq())
+	if err != nil {
+		return nil, err
+	}
 	return &Pump{Radio: r}, nil
+}
+
+func defaultFreq() uint32 {
+	freq := cc1100.DefaultFrequency
+	f := os.Getenv(freqEnvVar)
+	if len(f) == 0 {
+		return freq
+	}
+	n, err := fmt.Sscanf(f, "%d", &freq)
+	if err != nil {
+		log.Fatalf("%s value (%s): %v\n", freqEnvVar, f, err)
+	}
+	if n != 1 || freq < 860000000 || freq > 920000000 {
+		log.Fatalf("%s value (%s) should be the pump frequency in Hz\n", freqEnvVar, f)
+	}
+	return freq
 }
