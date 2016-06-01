af970c841e3c62cea5fdea21e637261d9b466f45
Eric Cooper <ecc@cmu.edu>
2016-06-01 15:06:43 -0400

    Add warning about interrupts

--- apps/oscillator.go
+++ apps/oscillator.go
@@ -1,5 +1,13 @@
 package main
 
+// Route the CC1101 clock oscillator (divided by 24) to the GDO0 pin,
+// so that its frequency can be measured easily with an oscilloscope
+// or frequency counter.
+
+// IMPORTANT: disconnect the Edison GPIO used for interrupts before
+// running this program.  Otherwise the Edison will be interrupted
+// at 1 MHz and become non-responsive.
+
 import (
 	"log"
 
@@ -13,6 +21,7 @@ func main() {
 	}
 	r.Reset()
 	// Route CLK_XOSC/24 to GDO0 pin.
+	// See data sheet, Table 41.
 	err = r.WriteRegister(cc1100.IOCFG0, 0x39)
 	if err != nil {
 		log.Fatal(err)
