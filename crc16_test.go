b65c03d0e8acbc1cacc107f355aec42f63120cb6
Eric Cooper <ecc@cmu.edu>
2016-06-18 19:35:56 -0400

    Add CRC-16 functions

---
+++ crc16_test.go
@@ -0,0 +1,21 @@
+package medtronic
+
+import (
+	"testing"
+)
+
+func TestCrc16(t *testing.T) {
+	cases := []struct {
+		msg []byte
+		sum uint16
+	}{
+		{[]byte{0x02, 0x06, 0x06, 0x03}, 0x41CD},
+		{[]byte{0x02, 0x09, 0x00, 0x05, 0x0D, 0x02, 0x03}, 0x71DA},
+	}
+	for _, c := range cases {
+		sum := Crc16(c.msg)
+		if sum != c.sum {
+			t.Errorf("Crc16(% X) == %X, want %X", c.msg, sum, c.sum)
+		}
+	}
+}
