3b844d7939601d781e317350cd093a1f4d13db51
Eric Cooper <ecc@cmu.edu>
2016-06-26 22:12:33 -0400

    Handle model-specific differences in response format

--- carbratios.go
+++ carbratios.go
@@ -10,7 +10,8 @@ const (
 
 type CarbRatio struct {
 	Start time.Duration // offset from 00:00:00
-	Carbs int           // grams or exchanges covered by one insulin unit
+	Carbs int           // grams or exchanges covered by TEN insulin units
+	Units CarbUnitsType
 }
 
 type CarbRatioSchedule struct {
@@ -18,27 +19,53 @@ type CarbRatioSchedule struct {
 }
 
 func (pump *Pump) CarbRatios() CarbRatioSchedule {
+	// Format of response depends on the pump family.
+	newer := pump.Family() >= 23
 	data := pump.Execute(CarbRatios)
 	if pump.Error() != nil {
 		return CarbRatioSchedule{}
 	}
-	if len(data) < 2 || (data[0]-1)%2 != 0 {
-		pump.BadResponse(CarbRatios, data)
-		return CarbRatioSchedule{}
-	}
-	n := (data[0] - 1) / 2
-	i := 2
-	// units := CarbUnitsInfo(data[1])
 	info := []CarbRatio{}
-	for n != 0 {
-		start := scheduleToDuration(data[i])
-		value := int(data[i+1])
-		info = append(info, CarbRatio{
-			Start: start,
-			Carbs: value,
-		})
-		n--
-		i += 2
+	units := CarbUnitsType(data[1])
+	if newer {
+		if len(data) < 2 || (data[0]-1)%3 != 0 {
+			pump.BadResponse(CarbRatios, data)
+			return CarbRatioSchedule{}
+		}
+		n := (data[0] - 1) / 3
+		i := 3
+		for n != 0 {
+			if data[i] == 0 && len(info) != 0 {
+				break
+			}
+			start := scheduleToDuration(data[i])
+			value := twoByteInt(data[i+1 : i+3])
+			info = append(info, CarbRatio{
+				Start: start,
+				Carbs: value,
+				Units: units,
+			})
+			n--
+			i += 3
+		}
+	} else {
+		if len(data) < 2 || (data[0]-1)%2 != 0 {
+			pump.BadResponse(CarbRatios, data)
+			return CarbRatioSchedule{}
+		}
+		n := (data[0] - 1) / 2
+		i := 2
+		for n != 0 {
+			start := scheduleToDuration(data[i])
+			value := int(data[i+1])
+			info = append(info, CarbRatio{
+				Start: start,
+				Carbs: value,
+				Units: units,
+			})
+			n--
+			i += 2
+		}
 	}
 	return CarbRatioSchedule{Schedule: info}
 }
