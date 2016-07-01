5ec10813781ed49efbcd31b60dd5824a20f02799
Eric Cooper <ecc@cmu.edu>
2016-06-30 22:53:35 -0400

    Let the json package handle base64-encoding of byte arrays

--- spilink/spilink.go
+++ spilink/spilink.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"io"
 	"log"
@@ -18,21 +17,19 @@ const (
 type (
 	SpiLinkCommand struct {
 		Command string
-		Data    string // hex-encoded
+		Data    []byte // base64-encoded by json.Marshal
 		Repeat  int
 		Timeout int // microseconds
 	}
 
 	SpiLinkResult struct {
-		Data  string // hex-encoded
+		Data  []byte // base64-encoded by json.Marshal
 		Rssi  int
 		Error bool
 	}
 )
 
 var (
-	version = "spilink 0.1"
-
 	input  = json.NewDecoder(os.Stdin)
 	output = json.NewEncoder(os.Stdout)
 
@@ -94,8 +91,8 @@ func (cmd SpiLinkCommand) perform() SpiLinkResult {
 	return result
 }
 
-func send(data string, repeat int) SpiLinkResult {
-	packet := medtronic.EncodePacket(hexDecode(data))
+func send(data []byte, repeat int) SpiLinkResult {
+	packet := medtronic.EncodePacket(data)
 	if repeat == 0 {
 		repeat = 1
 	}
@@ -120,7 +117,7 @@ func receive(timeout time.Duration) SpiLinkResult {
 	packet, rssi := pump.Radio.Receive(timeout)
 	data, err := medtronic.Decode6b4b(packet)
 	pump.SetError(err)
-	result.Data = hexEncode(data)
+	result.Data = data
 	if pump.Error() != nil {
 		log.Printf("%v", pump.Error())
 		pump.SetError(nil)
@@ -131,45 +128,4 @@ func receive(timeout time.Duration) SpiLinkResult {
 		log.Printf("received %d-byte packet (RSSI = %d)", len(packet), rssi)
 	}
 	return result
-}
-
-// The hexEncode and hexDecode functions correspond to
-// the Python encode('hex') and decode('hex') string methods,
-// so that arbitrary byte arrays can be transferred as JSON strings.
-
-var (
-	hexChar = []byte{
-		'0', '1', '2', '3', '4', '5', '6', '7',
-		'8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
-	}
-	hexDigit = map[byte]byte{}
-)
-
-func init() {
-	// Set up the inverse of the hexChar mapping for decoding.
-	for i, c := range hexChar {
-		hexDigit[c] = byte(i)
-	}
-
-}
-
-func hexEncode(data []byte) string {
-	var buf bytes.Buffer
-	for _, b := range data {
-		buf.WriteByte(hexChar[b>>4])
-		buf.WriteByte(hexChar[b&0xF])
-	}
-	return buf.String()
-}
-
-func hexDecode(str string) []byte {
-	var buf bytes.Buffer
-	n := len(str)
-	if n%2 != 0 {
-		log.Panicf("odd-length hex-encoded string (%s)", str)
-	}
-	for i := 0; i < n; i += 2 {
-		buf.WriteByte(hexDigit[str[i]]<<4 | hexDigit[str[i+1]])
-	}
-	return buf.Bytes()
 }
