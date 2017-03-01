ec3462decbd2f46ac78e0c79479fdb1e6b975228
Eric Cooper <ecc@cmu.edu>
2017-02-28 20:48:30 -0500

    Add command-line program to decode history records

---
+++ apps/printrecord.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+
+	"github.com/ecc1/medtronic"
+)
+
+var (
+	model = flag.Int("m", 523, "pump model")
+)
+
+func main() {
+	flag.Parse()
+	newer := *model%100 > 22
+	for _, arg := range flag.Args() {
+		data, err := base64.StdEncoding.DecodeString(arg)
+		if err != nil {
+			fmt.Printf("base64 decoding error: %v\n", err)
+			continue
+		}
+		fmt.Printf("[ % X ]\n", data)
+		r, err := medtronic.DecodeHistoryRecord(data, newer)
+		if err != nil {
+			fmt.Printf("decoding error: %v\n", err)
+			continue
+		}
+		b, err := json.MarshalIndent(r, "", "  ")
+		if err != nil {
+			fmt.Printf("marshaling error: %v\n", err)
+			continue
+		}
+		fmt.Println(string(b))
+
+	}
+}
