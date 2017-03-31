ab294cf6076ce4b925ea3c3e5784445ebe6cce11
Eric Cooper <ecc@cmu.edu>
2017-03-30 20:01:08 -0400

    Rename test case fields

--- packet/encoding_test.go
+++ packet/encoding_test.go
@@ -5,10 +5,10 @@ import (
 	"testing"
 )
 
-func TestEncoding(t *testing.T) {
+func Test4b6bEncoding(t *testing.T) {
 	cases := []struct {
-		src []byte
-		dst []byte
+		decoded []byte
+		encoded []byte
 	}{
 		{[]byte{}, []byte{}},
 		{[]byte{0x00}, []byte{0x55, 0x50}},
@@ -20,15 +20,15 @@ func TestEncoding(t *testing.T) {
 		{[]byte{0xA7, 0x12, 0x34, 0x56, 0x8D, 0x00, 0xA6}, []byte{0xA9, 0x6C, 0x72, 0x8F, 0x49, 0x66, 0x68, 0xD5, 0x55, 0xAA, 0x60}},
 	}
 	for _, c := range cases {
-		result := Encode4b6b(c.src)
-		if !bytes.Equal(result, c.dst) {
-			t.Errorf("Encode4b6b(%X) == %X, want %X", c.src, result, c.dst)
+		result := Encode4b6b(c.decoded)
+		if !bytes.Equal(result, c.encoded) {
+			t.Errorf("Encode4b6b(% X) == % X, want % X", c.decoded, result, c.encoded)
 		}
-		result, err := Decode6b4b(c.dst)
+		result, err := Decode6b4b(c.encoded)
 		if err != nil {
-			t.Errorf("Decode6b4b(%X) == %v, want %X", c.dst, err, c.src)
-		} else if !bytes.Equal(result, c.src) {
-			t.Errorf("Decode6b4b(%X) == %X, want %X", c.dst, result, c.src)
+			t.Errorf("Decode6b4b(% X) == %v, want % X", c.encoded, err, c.decoded)
+		} else if !bytes.Equal(result, c.decoded) {
+			t.Errorf("Decode6b4b(% X) == % X, want % X", c.encoded, result, c.decoded)
 		}
 	}
 }
