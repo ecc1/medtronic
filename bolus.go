ed3c0fc68e43dc3a300b56b20845046875de8883
Eric Cooper <ecc@cmu.edu>
2017-12-21 21:55:50 -0500

    Make sure Bolus command is not repeated

--- bolus.go
+++ bolus.go
@@ -5,11 +5,15 @@ const (
 )
 
 // Bolus delivers the given amount of insulin as a bolus.
+// For safety, this command is not attempted more than once.
 func (pump *Pump) Bolus(amount Insulin) {
 	newer := pump.Family() >= 23
 	if newer {
 		panic("unimplemented")
 	}
-	n := byte(amount / 100)
-	pump.Execute(bolus, n)
+	b := byte(amount / 100)
+	n := pump.Retries()
+	defer pump.SetRetries(n)
+	pump.SetRetries(1)
+	pump.Execute(bolus, b)
 }
