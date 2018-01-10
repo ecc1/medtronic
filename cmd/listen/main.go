5a6680d819ed741d52b78d1a87f72e32a029fa44
Eric Cooper <ecc@cmu.edu>
2018-01-10 11:25:18 -0500

    Add program to listen for a packet

---
+++ cmd/listen/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	"github.com/ecc1/medtronic"
+	"github.com/ecc1/medtronic/packet"
+)
+
+var (
+	listenDuration = flag.Duration("t", time.Hour, "max `duration` to listen")
+)
+
+func main() {
+	flag.Parse()
+	pump := medtronic.Open()
+	if pump.Error() != nil {
+		log.Fatal(pump.Error())
+	}
+	defer pump.Close()
+	log.Printf("listening for %v", *listenDuration)
+	p, rssi := pump.Radio.Receive(*listenDuration)
+	if pump.Error() != nil {
+		log.Fatal(pump.Error())
+	}
+	if len(p) == 0 {
+		log.Fatal("timeout")
+	}
+	log.Printf("raw data: % X (RSSI = %d)", p, rssi)
+	data, err := packet.Decode(p)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	log.Printf("decoded:  % X", data)
+}
