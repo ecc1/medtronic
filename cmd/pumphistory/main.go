3950e4b2577e0ecd822e6857eaf237f59e2df1d8
Eric Cooper <ecc@cmu.edu>
2018-01-23 17:00:26 -0500

    Add -s flag to fetch history since a given time

--- cmd/pumphistory/main.go
+++ cmd/pumphistory/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/ecc1/medtronic"
@@ -11,18 +12,28 @@ import (
 )
 
 var (
-	all      = flag.Bool("a", false, "get entire pump history")
-	numHours = flag.Int("n", 6, "number of `hours` of history to get")
-	nsFlag   = flag.Bool("t", false, "format as Nightscout treatments")
+	all       = flag.Bool("a", false, "get entire pump history")
+	numHours  = flag.Int("n", 6, "number of `hours` of history to get")
+	nsFlag    = flag.Bool("t", false, "format as Nightscout treatments")
+	sinceFlag = flag.String("s", "", "get history since the specified `time` in RFC3339 format")
 )
 
 func main() {
 	flag.Parse()
-	cutoff := time.Time{}
+	var cutoff time.Time
 	if *all {
 		log.Printf("retrieving entire pump history")
+	} else if *sinceFlag != "" {
+		var err error
+		cutoff, err = time.Parse(medtronic.JSONTimeLayout, *sinceFlag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
 	} else {
 		cutoff = time.Now().Add(-time.Duration(*numHours) * time.Hour)
+	}
+	if !*all {
 		log.Printf("retrieving pump history since %s", cutoff.Format(medtronic.UserTimeLayout))
 	}
 	pump := medtronic.Open()
