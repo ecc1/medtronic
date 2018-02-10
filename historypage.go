d79e658309273eb38e28bc8178173c387b1ec616
Eric Cooper <ecc@cmu.edu>
2018-02-10 17:31:18 -0500

    Add special case for model x12 pumps in HistoryPageCount

--- historypage.go
+++ historypage.go
@@ -14,6 +14,11 @@ func (pump *Pump) HistoryPage(page int) []byte {
 func (pump *Pump) HistoryPageCount() int {
 	data := pump.Execute(historyPageCount)
 	if pump.Error() != nil {
+		e, ok := pump.Error().(InvalidCommandError)
+		if ok && e.PumpError == CommandRefused && pump.Family() == 12 {
+			pump.SetError(nil)
+			return 32
+		}
 		return 0
 	}
 	if len(data) < 5 || data[0] != 4 {
