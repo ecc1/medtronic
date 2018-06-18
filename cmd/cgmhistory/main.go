756d7fdd76b36eb0bc705a2ac54b7361d6527fb6
Jens Heuschkel <heuschkel@tk.tu-darmstadt.de>
2018-06-18 11:10:25 +0200

    update cgmhistory to output legacy format to be formatted by openaps

--- cmd/cgmhistory/main.go
+++ cmd/cgmhistory/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"os"
 
 	"github.com/ecc1/medtronic"
 	"github.com/ecc1/nightscout"
@@ -13,8 +14,10 @@ import (
 var (
 	all       = flag.Bool("a", false, "get entire CGM history")
 	numHours  = flag.Int("n", 6, "number of `hours` of history to get")
-	nsFlag    = flag.Bool("e", false, "format as Nightscout entries")
+	nsFlag    = flag.Bool("e", false, "force output in stdout and format as Nightscout entries")
 	sinceFlag = flag.String("s", "", "get history since the specified `time` in RFC3339 format")
+	jsonFile  = flag.String("l", "", "append results to `file` in legacy format")
+	nsFile    = flag.String("ns", "", "append results to `file` in Nightscout format")
 )
 
 func main() {
@@ -34,6 +37,7 @@ func main() {
 	if !*all {
 		log.Printf("retrieving pump history since %s", cutoff.Format(medtronic.UserTimeLayout))
 	}
+	
 	pump := medtronic.Open()
 	defer pump.Close()
 	pump.Wakeup()
@@ -41,10 +45,36 @@ func main() {
 	if pump.Error() != nil {
 		log.Fatal(pump.Error())
 	}
+	
 	if *nsFlag {
 		medtronic.ReverseCGMHistory(results)
 		fmt.Println(nightscout.JSON(medtronic.Entries(results)))
-	} else {
+	} else if *jsonFile == "" && *nsFile == "" {
 		fmt.Println(nightscout.JSON(results))
+	}
+	
+	if *jsonFile != "" {
+		formattedData := medtronic.FormatToOAPS(results)
+		jsonData := nightscout.JSON(formattedData)
+		f, err := os.Create(*jsonFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		f.WriteString(jsonData)
+		log.Printf("wrote %d entries to %s", len(formattedData), *jsonFile)
+	}
+	
+	if *nsFile != "" {
+		medtronic.ReverseCGMHistory(results)
+		enties := medtronic.Entries(results)
+		jsonData := nightscout.JSON(enties)
+		f, err := os.Create(*nsFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		f.WriteString(jsonData)
+		log.Printf("wrote %d entries to %s", len(enties), *nsFile)
 	}
 }
